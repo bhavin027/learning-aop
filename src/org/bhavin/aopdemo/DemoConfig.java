f0220399c5f738f533a0bad193860d39a6c8fd3e
bhavin027 <bhavinghoghari27@gmail.com>
2020-10-30 19:20:41 +0530

    Basic Spring AOP setup using java class annotation(No XML) config

---
+++ src/org/bhavin/aopdemo/DemoConfig.java
@@ -0,0 +1,12 @@
+package org.bhavin.aopdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan("org.bhavin.aopdemo")
+public class DemoConfig {
+	
+}
---
+++ src/org/bhavin/aopdemo/dao/AccountDAO.java
@@ -0,0 +1,13 @@
+package org.bhavin.aopdemo.dao;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class AccountDAO {
+	
+	public void addAccount() {
+		
+		System.out.println(getClass() +": Doing my DB work- Adding an account...");
+		
+	}
+}
