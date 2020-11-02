76d0e501d2013fcb4b07979692e5cd308594b5b2
bhavin027 <bhavinghoghari27@gmail.com>
2020-11-02 21:31:22 +0530

    Implemented AOP @After(finally) method

--- src/org/bhavin/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/org/bhavin/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -3,6 +3,7 @@ package org.bhavin.aopdemo.aspect;
 import java.util.List;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
@@ -85,4 +86,14 @@ public class MyDemoLoggingAspect {
 		//log the exception
 		System.out.println("Exception: "+theExc);
 	}
+	
+	@After("execution(* org.bhavin.aopdemo.dao.AccountDAO.findAccounts(..))")
+	public void afterFinallyFindAccountsAdvice(JoinPoint theJoinPoint) {
+		//print method we are advising on
+		String method = theJoinPoint.getSignature().toShortString();
+		System.out.println("\n====>>>Executing @After(finally) on method: "+method);
+		
+	}
+	
+	
 }
